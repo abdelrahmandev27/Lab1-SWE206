01b99c9792541940df8439ece2d9317cef4ea050
Mona Lisa <s202444200@kfupm.edu.sa>
2025-09-07 14:30:11 +0300

    initial feature commit

--- test.cs
+++ test.cs
@@ -5,11 +5,10 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello World!");
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello 1!");
+        Console.WriteLine("Hello 2!");
+        Console.WriteLine("Hello 3!");
+        Console.WriteLine("Hello 4!");
+        Console.WriteLine("Hello 5!");
     }
 }
