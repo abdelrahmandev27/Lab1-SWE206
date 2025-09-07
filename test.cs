55b03d9c130e6c56b86242cd6f77a29397af12ab
Mona Lisa <s202444200@kfupm.edu.sa>
2025-09-07 15:00:18 +0300

    change 2

--- test.cs
+++ test.cs
@@ -5,7 +5,7 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello World!");
-        Console.WriteLine("Hello 1!");
+        Console.WriteLine("Hello 1");
         Console.WriteLine("Hello 2!");
         Console.WriteLine("Hello 3!");
         Console.WriteLine("Hello 4!");
