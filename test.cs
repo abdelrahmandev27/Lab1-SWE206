e6be25d285814fa4933a52c1470214a72eae015e
student <student@ccm.kfupm.edu.sa>
2025-09-07 14:27:17 +0300

    added few hello worlds

--- test.cs
+++ test.cs
@@ -1,8 +1,15 @@
 using System;
 
-class Program {
-
-    static void Main() {
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
         Console.WriteLine("Hello World!");
     }
 }
